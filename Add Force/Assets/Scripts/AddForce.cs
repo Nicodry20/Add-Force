a9148d312b4dabbb3cbbb97572e5e3803f8b5435
Nicodry20 <48178309@est.ort.edu.ar>
2024-05-13 15:56:29 -0300

    Add Force

---
+++ Add Force/Assets/Scripts/AddForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForce : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //al presionar el Espacio el cubo salte
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Add Force/Assets/Scripts/CannonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public float shootingForce;
+    public float rotationSpeed;
+    public Transform firePoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(- rotationSpeed * Time.deltaTime, 0, 0);
+        } 
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
